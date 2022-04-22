e53a426dc7813e260814a8c4109c73f92abacc4b
Grisha Angelov <grisha.angelov89@gmail.com>
2022-04-22 15:58:03 +0300

    Add SpeechRecognition function check

--- src/components/VoiceMap/recognition.js
+++ src/components/VoiceMap/recognition.js
@@ -1,8 +1,12 @@
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-const recognition = new SpeechRecognition();
-recognition.continuous = false;
-recognition.lang = 'en-US';
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
+let recognition = {}
+
+if (typeof SpeechRecognition === 'function') {
+    recognition = new SpeechRecognition();
+    recognition.continuous = false;
+    recognition.lang = 'en-US';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+}
 
 export default recognition
