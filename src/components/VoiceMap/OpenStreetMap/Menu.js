4f6b1162d4c1ea8308097f4bc9ac5b04aa5e2c17
Grisha Angelov <grisha.angelov89@gmail.com>
2022-04-23 17:04:25 +0300

    Open Drawer when click on icons in Menu

--- src/components/VoiceMap/OpenStreetMap/Menu.js
+++ src/components/VoiceMap/OpenStreetMap/Menu.js
@@ -1,12 +1,23 @@
+import { useState } from 'react'
+
 import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
+import Drawer from '@mui/material/Drawer'
 
 import HistoryIcon from '@mui/icons-material/History'
 import DesktopWindowsIcon from '@mui/icons-material/DesktopWindows'
 
 const Menu = () => {
-    const handleChange = (event, index) => {
+    const [showHistory, setShowHistory] = useState(false)
+    const [showWorkflow, setShowWorkflow] = useState(false)
 
+    const handleChange = (event, index) => {
+        index === 0 ? setShowHistory(true) : setShowWorkflow(true)
+    }
+
+    const handleClose = () => {
+        setShowHistory(false)
+        setShowWorkflow(false)
     }
 
     return (
@@ -16,6 +27,11 @@ const Menu = () => {
                     <BottomNavigationAction label="History" icon={<HistoryIcon />} />
                     <BottomNavigationAction label="Workflow" icon={<DesktopWindowsIcon />} />
                 </BottomNavigation>
+
+                <Drawer open={showHistory || showWorkflow} anchor={'top'} onClose={handleClose}>
+                    {showHistory && <div>HISTORY</div>}
+                    {showWorkflow && <div>WORKFLOW</div>}
+                </Drawer>
             </div>
         </div>
     )
