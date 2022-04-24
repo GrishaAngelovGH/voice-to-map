ab421033fdaa68316f0559a59ebd8576ce385151
Grisha Angelov <grisha.angelov89@gmail.com>
2022-04-24 16:25:14 +0300

    Visualize history in Dialog

--- src/components/VoiceMap/OpenStreetMap/Menu/History.js
+++ src/components/VoiceMap/OpenStreetMap/Menu/History.js
@@ -1,40 +1,52 @@
 import { useSelector } from 'react-redux'
+import { Fragment } from 'react'
 
-import Box from '@mui/material/Box'
+import Dialog from '@mui/material/Dialog'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
+import Divider from '@mui/material/Divider'
 
 import RoomIcon from '@mui/icons-material/Room'
+import CloseIcon from '@mui/icons-material/Close'
 
-const History = () => {
+const History = ({ onClose }) => {
     const history = useSelector(state => state.locations.history)
 
     return (
-        <div className="row g-0" style={{ height: 300 }}>
-            <div className="col-md-12">
-                <Box sx={{ bgcolor: 'background.paper' }}>
-                    <nav aria-label="main mailbox folders">
-                        <List>
-                            {
-                                history.map(v => (
-                                    <ListItem key={v.place_id} disablePadding>
-                                        <ListItemButton>
-                                            <ListItemIcon>
-                                                <RoomIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary={v.display_name} />
-                                        </ListItemButton>
-                                    </ListItem>
-                                ))
-                            }
-                        </List>
-                    </nav>
-                </Box>
-            </div>
-        </div >
+        <Dialog fullScreen={true} open={true}>
+            <AppBar sx={{ position: 'sticky' }}>
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        onClick={onClose}
+                        aria-label="close"
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                    <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                        History
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <List>
+                {
+                    history.map((v, i) => (
+                        <Fragment key={v.place_id}>
+                            <ListItem button>
+                                <ListItemText primary={<span>{`#${i + 1}`} <RoomIcon /> {v.display_name}</span>} />
+                            </ListItem>
+                            <Divider />
+                        </Fragment>
+                    ))
+                }
+            </List>
+        </Dialog>
     )
 }
 
