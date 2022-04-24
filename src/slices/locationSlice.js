241caae8803590821c1c0572405df8c2a4290d33
Grisha Angelov <grisha.angelov89@gmail.com>
2022-04-24 15:29:42 +0300

    Add reducer note in locationSlice

--- src/slices/locationSlice.js
+++ src/slices/locationSlice.js
@@ -8,6 +8,9 @@ const initialState = {
 export const locationSlice = createSlice({
     name: 'location',
     initialState,
+    // Redux Toolkit allows us to write "mutating" logic in reducers. 
+    // It doesn't actually mutate the state.
+    // https://redux-toolkit.js.org/tutorials/quick-start#create-a-redux-state-slice
     reducers: {
         addLocation: (state, action) => {
             state.lastLocation = action.payload
