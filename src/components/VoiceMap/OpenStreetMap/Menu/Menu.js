641ae3b004ec2529100b380969534033381f0c8c
Grisha Angelov <grisha.angelov89@gmail.com>
2022-04-24 16:26:40 +0300

    Add suggestions button to Menu

--- src/components/VoiceMap/OpenStreetMap/Menu/Menu.js
+++ src/components/VoiceMap/OpenStreetMap/Menu/Menu.js
@@ -4,6 +4,7 @@ import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import Drawer from '@mui/material/Drawer'
 
+import ListIcon from '@mui/icons-material/List'
 import HistoryIcon from '@mui/icons-material/History'
 import DesktopWindowsIcon from '@mui/icons-material/DesktopWindows'
 
@@ -11,14 +12,21 @@ import History from './History'
 
 const Menu = ({ disableHistory }) => {
     const [showHistory, setShowHistory] = useState(false)
+    const [showSuggestions, setShowSuggestions] = useState(false)
     const [showWorkflow, setShowWorkflow] = useState(false)
 
     const handleChange = (event, index) => {
-        index === 0 ? !disableHistory && setShowHistory(true) : setShowWorkflow(true)
+        if (index === 0) {
+            !disableHistory && setShowHistory(true)
+            return
+        }
+
+        index === 1 ? setShowSuggestions(true) : setShowWorkflow(true)
     }
 
     const handleClose = () => {
         setShowHistory(false)
+        setShowSuggestions(false)
         setShowWorkflow(false)
     }
 
@@ -27,11 +35,13 @@ const Menu = ({ disableHistory }) => {
             <div className="col-md-12">
                 <BottomNavigation showLabels={true} onChange={handleChange}>
                     <BottomNavigationAction label="History" icon={<HistoryIcon />} />
+                    <BottomNavigationAction label="Suggestions" icon={<ListIcon />} />
                     <BottomNavigationAction label="Workflow" icon={<DesktopWindowsIcon />} />
                 </BottomNavigation>
 
-                <Drawer open={showHistory || showWorkflow} anchor={'top'} onClose={handleClose}>
-                    {showHistory && <History />}
+                <Drawer open={showHistory || showWorkflow || showSuggestions} anchor={'top'} onClose={handleClose}>
+                    {showHistory && <History onClose={handleClose} />}
+                    {showSuggestions && <div>SUGGESTIONS</div>}
                     {showWorkflow && <div>WORKFLOW</div>}
                 </Drawer>
             </div>
